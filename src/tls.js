6fc34475382f11df10db09661cea1c6786f1b9dc
Dean Netherton <dnetherton@dius.com.au>
2019-07-20 14:58:40 +1000

    Updated createTlsMqttClient to support file path for certs/key/ca options

--- src/tls.js
+++ src/tls.js
@@ -1,17 +1,24 @@
 import mqtt from 'mqtt'
 import tls from 'tls'
+import fs from 'fs'
 
 function tlsConnection(options) {
+
+  const ca = options.ca || fs.readFileSync(options.caPath)
+  const key = options.key || fs.readFileSync(options.keyPath)
+  const cert = options.cert || fs.readFileSync(options.certPath)
+
   const mqttClientOptions = {
     port: 8883,
     enableTrace: false,
     requestCert: true,
     rejectUnauthorized: true,
-    ca: options.ca,
-    key: options.key,
-    cert: options.cert,
-    ...options
+    ...options,
+    ca,
+    key,
+    cert
   }
+
   return mqttClient => {
     const tlsConnection = tls.connect(mqttClientOptions)
 
@@ -32,7 +39,7 @@ function tlsConnection(options) {
   }
 }
 
-export async function createTlsMqttClient(options) {
-  const tlsConnector = await tlsConnection(options)
+export function createTlsMqttClient(options) {
+  const tlsConnector = tlsConnection(options)
   return new mqtt.MqttClient(tlsConnector, options)
 }
